500a5785e1b4685f6ee354e064736c59cc79fdee
orbdev1 <ing.ryqm@gmail.com>
2025-02-03 23:10:06 -0500

    Creaccion de proyecto 5 y configuracion con tailwind

---
+++ 05-product-preview-card/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-cream h-screen w-screen flex justify-center items-center">
+        <article className="flex flex-col"></article>
+      </div>
+    </>
+  );
+}
+
+export default App;
