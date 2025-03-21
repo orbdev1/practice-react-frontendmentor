afe77416cf9c012338e86d8b60c23f662a3e683c
orbdev1 <ing.ryqm@gmail.com>
2025-03-20 23:00:45 -0500

    Avance proeycto 8, mobile terminado

--- 08-article-preview-component-master/src/components/Article.tsx
+++ 08-article-preview-component-master/src/components/Article.tsx
@@ -1,31 +1,59 @@
+import { useState } from "react";
 import ShareLinks from "./ShareLinks";
 import UserProfile from "./UserProfile";
 
 export default function Article() {
+  const [isShareOpen, setIsShareOpen] = useState(false);
+
   return (
-    <section className="bg-white mx-6 my-auto rounded-2xl overflow-hidden shadow-lg min-h-[510px]">
-      <div className="h-[40%] w-full">
+    <section className="bg-white mx-6 rounded-2xl overflow-hidden shadow-lg flex flex-col max-w-[550px] md:flex-row md:max-w-[700px]">
+      <div className="h-50 w-full md:h-auto md:w-[80%]">
         <img
-          className="w-full h-full object-cover object-[50%_20%]"
+          className="w-full h-full object-cover object-[50%_20%] md:object-left"
           src="/drawers.jpg"
           alt="Image of Drawers"
         />
       </div>
-      <div className="p-[32px] pb-5">
-        <div>
+      <div className="">
+        <div className="p-8 pb-0">
           <h2 className="text-very-dark-grayish-blue text-[17px] leading-6">
             Shift the overall look and feel by adding these wonderful touches to
             furniture in your home
           </h2>
-          <p className="text-desaturated-dark-blue text-[13.3px] mt-4 mb-6 leading-5">
+          <p className="text-desaturated-dark-blue text-[13.3px] mt-4 mb-4 leading-5">
             Ever been in a room and felt like something was missing? Perhaps it
             felt slightly bare and uninviting. I’ve got some simple tips to help
             you make any room feel complete.
           </p>
         </div>
-        <div>
-          <UserProfile />
-          {/* <ShareLinks /> */}
+        <div
+          className={`${
+            isShareOpen && "bg-very-dark-grayish-blue"
+          } flex items-center justify-between px-8 h-16`}
+        >
+          {!isShareOpen ? <UserProfile /> : <ShareLinks />}
+          <div
+            onClick={() => {
+              setIsShareOpen(!isShareOpen);
+            }}
+            className={`${
+              !isShareOpen
+                ? "bg-light-grayish-blue"
+                : "bg-desaturated-dark-blue"
+            } rounded-full grid place-items-center aspect-square w-8`}
+          >
+            <svg
+              className={`w-[14px] h-[14px] ${
+                !isShareOpen
+                  ? "fill-desaturated-dark-blue"
+                  : "fill-light-grayish-blue"
+              }`}
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 15 13"
+            >
+              <path d="M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z" />
+            </svg>
+          </div>
         </div>
       </div>
     </section>
--- 08-article-preview-component-master/src/components/ShareLinks.tsx
+++ 08-article-preview-component-master/src/components/ShareLinks.tsx
@@ -1,3 +1,18 @@
-export default function ShareLinks({}: Props) {
-  return <div>ShareLinks</div>;
+export default function ShareLinks() {
+  return (
+    <div className="flex gap-4">
+      <span className="text-grayis-blue tracking-[4px]">SHARE</span>
+      <img
+        className="h-auto w-5"
+        src="/icon-facebook.svg"
+        alt="Logo Facebook"
+      />
+      <img className="h-auto w-5" src="/icon-twitter.svg" alt="Logo Twitter" />
+      <img
+        className="h-auto w-5"
+        src="/icon-pinterest.svg"
+        alt="Logo Pinterest"
+      />
+    </div>
+  );
 }
